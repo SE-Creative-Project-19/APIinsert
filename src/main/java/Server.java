eaa7f8fc62e7e962af89acfe7f7fa41515db3d8e
kitKimBeomSeok <126782965+kitKimBeomSeok@users.noreply.github.com>
2023-05-24 03:44:30 +0900

    serviceInfoDTO에 시도코드가 String이길래 int로 변경후 map을 사용하여 출력할때 시도번역 까지 해놨습니다.

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -15,7 +15,7 @@ public class Server {
 
             // 객체를 전송하기 위한 스트림 생성
             ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream());
-
+            ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream());
             // 객체 생성 및 직렬화
            ServiceInfoDTO serviceInfoDTO = new ServiceInfoDTO();
            serviceInfoDTO.setServiceInfoPK(1234);
@@ -23,7 +23,10 @@ public class Server {
            serviceInfoDTO.setSrvcCSCode("예시 상세분야");
            serviceInfoDTO.setEmail("as102302@naver.com");
            outputStream.writeObject(serviceInfoDTO);
-            outputStream.flush();
+            serviceInfoDTO = (ServiceInfoDTO) inputStream.readObject();
+            System.out.println("서버에서 받은 객체 정보:");
+            System.out.println("이름: " + serviceInfoDTO.toString());
+            //outputStream.flush();
             System.out.println("객체를 클라이언트로 전송하였습니다.");
 
             // 연결 종료
@@ -32,6 +35,8 @@ public class Server {
             serverSocket.close();
         } catch (IOException e) {
             e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
         }
     }
 }
