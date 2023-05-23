d4f824300e21751801b578299dad44638c402136
SIron17 <jsca10021@gmail.com>
2023-05-24 00:44:22 +0900

    UserDAO 수정 중

--- src/main/java/persistence/dao/UserDAO.java
+++ src/main/java/persistence/dao/UserDAO.java
@@ -1,37 +1,41 @@
-//package persistence.dao;
-//
-//import org.apache.ibatis.session.SqlSession;
-//import persistence.dto.UserDTO;
-//import org.apache.ibatis.session.SqlSessionFactory;
-//
-//import java.util.ArrayList;
-//import java.util.HashMap;
-//import java.util.List;
-//import java.util.Map;
-//
-//public class UserDAO {
-//    private SqlSession sqlSession;
-//    private final SqlSessionFactory sqlSessionFactory;
-//
-//    public UserDAO(SqlSession sqlSession, SqlSessionFactory sqlSessionFactory) {
-//        this.sqlSession = sqlSession;
-//        this.sqlSessionFactory = sqlSessionFactory;
-//    }
-//
-//    public List<UserDTO> getUser(String userId){
-//        SqlSession session = sqlSessionFactory.openSession();
-//        try{
-//            session.insert("mapper.UserMapper.UserMapper", )
-//        }
-//    }
-//
-//    public void insertUserInfo(UserDTO userDTO){
-//        SqlSession session = sqlSessionFactory.openSession();
-//        try{
-//            session.insert("mapper.UserMapper.UserMapper", userDTO);
-//            session.commit();
-//        } finally {
-//            session.close();
-//        }
-//    }
-//}
+package persistence.dao;
+
+import org.apache.ibatis.session.SqlSession;
+import persistence.dto.UserDTO;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class UserDAO {
+    private SqlSession sqlSession;
+    private final SqlSessionFactory sqlSessionFactory;
+
+    public UserDAO(SqlSession sqlSession, SqlSessionFactory sqlSessionFactory) {
+        this.sqlSession = sqlSession;
+        this.sqlSessionFactory = sqlSessionFactory;
+    }
+
+    public List<UserDTO> getUser() {
+        List<UserDTO> list = null;
+        SqlSession session = sqlSessionFactory.openSession();
+        try{
+            list = session.selectList("mapper.UserMapper.getUser");
+        } finally {
+            session.close();
+        }
+        return list;
+    }
+
+    public void insertUser(UserDTO userDTO){
+        SqlSession session = sqlSessionFactory.openSession();
+        try{
+            session.insert("mapper.UserMapper.insertUser",userDTO);
+            session.commit();
+        } finally {
+            session.close();
+        }
+    }
+}
