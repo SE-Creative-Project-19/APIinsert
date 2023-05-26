0b4a5daa051d0dadf6c620499b2dd9c2c7ea6a17
SIron17 <jsca10021@gmail.com>
2023-05-26 14:47:35 +0900

    test

--- src/main/java/view/VolunteerView.java
+++ src/main/java/view/VolunteerView.java
@@ -12,7 +12,7 @@ public class VolunteerView {
 
     public VolunteerView(VolunteerService service) {
         this.service = service;
-    }
+    } // test 입니당
 
     public void printVolunteer(List<VolunteerDTO> list){
         for(VolunteerDTO volunteerDTO : list) {
