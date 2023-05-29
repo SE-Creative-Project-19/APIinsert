b0ca3e4d2a74b42dca1b15cf2d65908eaeeafd13
SIron17 <jsca10021@gmail.com>
2023-05-29 21:01:06 +0900

    User 회원가입, 로그인, 회원정보조회 통신 완료 Server ID 찾기, 비밀번호 재설정 오류 터짐

--- src/main/java/persistence/dto/UserDTO.java
+++ src/main/java/persistence/dto/UserDTO.java
@@ -12,25 +12,25 @@ public class UserDTO implements Serializable {
     private int userPK; //사용자PK
     private String ID;
     private String PW;
-    private String phoneNumber;
-    private String address;
-    private int mannerTemperature; //매너온도
-    private int type; //사용자 유형
-    private String name;
-    private String facility; //TODO serviceInfo 테이블의 mnnstNm를가짐
+    private String PhoneNumber;
+    private String Address;
+    private int MannerTemperature; //매너온도
+    private int Type; //사용자 유형
+    private String Name;
+    private String Facility; // serviceInfo 테이블의 mnnstNm를가짐
 
     public UserDTO() {
     }
 
-    public UserDTO(int userPK, String ID, String PW, String phoneNumber, String address, int mannerTemperature, int type, String name, String facility) {
+    public UserDTO(int userPK, String ID, String PW, String PhoneNumber, String Address, int MannerTemperature, int Type, String Name, String Facility) {
         this.userPK = userPK;
         this.ID = ID;
         this.PW = PW;
-        this.phoneNumber = phoneNumber;
-        this.address = address;
-        this.mannerTemperature = mannerTemperature;
-        this.type = type;
-        this.name = name;
-        this.facility = facility;
+        this.PhoneNumber = PhoneNumber;
+        this.Address = Address;
+        this.MannerTemperature = MannerTemperature;
+        this.Type = Type;
+        this.Name = Name;
+        this.Facility = Facility;
     }
 }
--- src/main/java/service/VolunteerService.java
+++ src/main/java/service/VolunteerService.java
@@ -14,22 +14,22 @@ public class VolunteerService{
 
     public List<VolunteerDTO> getVolunteer() {return dao.getVolunteer();}
 
-    public List<VolunteerDTO> getVolunteerFilter(VolunteerDTO volunteerDTO, String processingResult){//TODO 봉사 신청자 명단을 바탕으로 해당 유저의 정보들을 리스트로 반환
+    public List<VolunteerDTO> getVolunteerFilter(VolunteerDTO volunteerDTO, String processingResult){// 봉사 신청자 명단을 바탕으로 해당 유저의 정보들을 리스트로 반환
         return dao.getVolunteerFilter(volunteerDTO, processingResult);
     }
-    public List<UserDTO> getVolunteerListByServicePK(List<VolunteerDTO> volunteerDTOS, int pageNo){ //TODO 봉사 신청 리스트에서  신청자에 대한  userDTO를 리스트  return
+    public List<UserDTO> getVolunteerListByServicePK(List<VolunteerDTO> volunteerDTOS, int pageNo){ // 봉사 신청 리스트에서  신청자에 대한  userDTO를 리스트 return
        List<UserDTO> userDTOS = userService.getUsersByPK(volunteerDTOS, pageNo);
        return userDTOS;
     }
-    public List<VolunteerDTO> getVolunteerApplicant(String facility) {//TODO 담당자가 해당 기관의 승인이 필요한 봉사 신청 리스트를 return
+    public List<VolunteerDTO> getVolunteerApplicant(String facility) {// 담당자가 해당 기관의 승인이 필요한 봉사 신청 리스트를 return
         return dao.getVolunteerApplicant(facility);
     }
 
-    public void updateVolunteer(VolunteerDTO volunteerDTO) {//TODO 봉사처리 상태를 수정하는 용도로 사용합니다.
+    public void updateVolunteer(VolunteerDTO volunteerDTO) {// 봉사처리 상태를 수정하는 용도로 사용합니다.
          dao.updateVolunteer(volunteerDTO);
     }
 
-    public List<VolunteerDTO> getVolunteerDone(String facility) { //TODO 봉사기간이 지나서 봉사는 완료되었으나 별점을 등록하지 않은 봉사 신청 리스트를  return
+    public List<VolunteerDTO> getVolunteerDone(String facility) { // 봉사기간이 지나서 봉사는 완료되었으나 별점을 등록하지 않은 봉사 신청 리스트를 return
         return dao.getVolunteerDone(facility);
     }
 }
